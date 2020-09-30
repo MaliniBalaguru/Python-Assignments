90784eb0e69446d5788e31cc7d402ec529584589
MaliniBalaguru <72144912+MaliniBalaguru@users.noreply.github.com>
2020-09-30 14:33:32 -0400

    Add files via upload

---
+++ Arrow_Head.py
@@ -0,0 +1,20 @@
+#This creates an arrowhead for a list of odd numbers up to value n
+
+
+n = int(input("Enter any integer: "))
+i = 1
+x = 1
+odd_numbers = []
+while i <= n:
+    if x % 2 != 0:
+        odd_numbers.append(x)
+    x += 1
+    i += 1
+
+for odd_counter in odd_numbers:
+    output = " "
+    for count in range(odd_counter):
+        output += "x"
+    print(output.center(max(odd_numbers)))
+
+
