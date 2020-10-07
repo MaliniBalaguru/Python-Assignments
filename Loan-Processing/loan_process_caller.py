fc844dd4f94bf178be7214100f38c778a5b7faa3
MaliniBalaguru <72144912+MaliniBalaguru@users.noreply.github.com>
2020-10-07 14:40:04 -0400

    Add files via upload

---
+++ Loan-Processing/loan_process_caller.py
@@ -0,0 +1,9 @@
+from loan_processing import process_loan
+
+process_loan()
+
+
+
+
+
+
---
+++ Loan-Processing/loan_processing.py
@@ -0,0 +1,29 @@
+# This program contains functions for processing a loan
+import Meta_Data
+
+
+def process_loan():
+# Get customer information
+    cust_name = input("Enter customer name: ")
+    req_loan = int(input("Enter requested loan amount: "))
+    credit_score = int(input("Enter credit score: "))
+
+
+# Look up meta data based on customer information
+    for criteria_dict in Meta_Data.metadata():
+        if req_loan <= criteria_dict["loan"] and criteria_dict["cs_start"] <= credit_score <= criteria_dict["cs_end"]:
+            print(f'Approved loan = ${criteria_dict["loan"]}\nInterest rate = {criteria_dict["interest_rate"]}%\nLoan Duration = {criteria_dict["duration"]} months')
+            break
+    else:
+        print("Loan not approved")
+
+
+
+
+
+
+
+
+
+
+
